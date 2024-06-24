f2cd25ac537d6bcfd444b1590f6a83f26d0ea589
Jacob <jacob.roling@hey.com>
2024-06-24 10:54:52 +0930

    chore: savepoint

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,7 @@
 /// <reference types="astro/client" />
 
 interface Window {
-<<<<<<< HEAD
-  htmx: import("htmx.org");
-=======
   up: any;
->>>>>>> 40dd981 (chore: savepoint)
   posthog: import("posthog-js");
   Stimulus: import("@hotwired/stimulus").Application;
   recalculateHeaderSpacing: import("#utils").recalculateHeaderSpacing;
